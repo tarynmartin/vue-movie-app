2b81553bcca6fbfe6f1febc37582bc1a61836e8f
Taryn Martin <taryn.martin919@gmail.com>
2024-02-29 11:47:32 -0700

    added proxy server for api requests and added getting data from api to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,35 @@
 <script setup>
 // need to import card component
 // get data on load
-import InputComponent from '../components/input/InputComponent.vue'
-import { ref } from 'vue'
+import axios from 'axios';
+import { ref, watch } from 'vue';
+import InputComponent from '../components/input/InputComponent.vue';
 
 const search = ref('');
+const releases = ref([]);
+
+watch(search, async() => {
+  const updatedParams = { params: {
+      endpoint: '',
+      query: '',
+    }}
+
+  if (!search.value) {
+    updatedParams.params.endpoint = 'releases/';
+  } else {
+    updatedParams.params.endpoint = `search/'`;
+    updatedParams.params.query = new URLSearchParams({ search_value: search.value }).toString();
+  }
+
+  try {
+    const response = await axios.get('http://localhost:5000', updatedParams );
+
+    releases.value = response.data.releases;
+  } catch (e) {
+    console.error('error', e);
+  }
+    
+}, { immediate: true })
 </script>
 
 <template>
